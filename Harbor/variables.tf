729378c682e11ddf21b30df1db80873305c2aab5
Florian Chaillou <tighter-bandage-5j@icloud.com>
2025-09-24 15:46:44 +0200

    feat: create variables.tf

---
+++ Harbor/variables.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t3.medium"
+}
+
+variable "key_name" {
+  description = "SSH key pair for EC2"
+  type        = string
+  default     = "chimera-key"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  default = "10.0.1.0/24"
+}
